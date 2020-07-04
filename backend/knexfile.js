14736a4ad8fc1b8f21351d3e5812dacffe44da5c
Darlan <darlan.nascimento16@gmail.com>
2020-07-04 17:15:36 -0300

    configuracao do knex e primeira migration

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,44 +1,54 @@
-// Update with your config settings.
-
 module.exports = {
-
+    
+  /*
+  client: 'postgresql',
+  connection: {
+      database: 'knowledge',
+      user: 'postgres',
+      password: '123456'
+  },
+  pool: {
+      min: 2,
+      max: 10
+  },
+  migrations: {
+      tableName: 'knex_migrations'
+  }*/
+  
+  /*
+  client: 'postgresql',
+  connection: {
+      host: 'codaxpostgres.c8rmjbl8lqg6.us-west-2.rds.amazonaws.com',
+      database: 'postgres',
+      user: 'codax',
+      password: 'codax2020'
+  },
+  pool: {
+      min: 2,
+      max: 10
+  },
+  migrations: {
+      tableName: 'knex_migrations'
+  }*/
   development: {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+        host: '85.10.205.173',
+        user: 'matheus_ebrains',
+        password: 'ebrainsteste',
+        database: 'ebrains'
     },
     pool: {
-      min: 2,
-      max: 10
+        min: 2,
+        max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+        tableName: 'knex_migrations',
+        directory: `${__dirname}/src/database/migrations`
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory:`${__dirname}/src/database/seeds`
     }
   }
-
+  
 };
